cf0d9427f604ef8d93dc5c600144fd6ab8ed7409
lynerist <leonardo.albani@studenti.unimi.it>
2021-12-12 23:45:14 +0100

    day 12, recursion is the way

---
+++ Day_12/day12_a.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main(){
+	//Read the file
+	input, _ := os.Open("input.txt")
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+
+	connections := make(map[string][]string)
+
+	for sc.Scan(){
+		path := strings.Split(sc.Text(), "-")
+		connections[path[0]] = append(connections[path[0]], path[1])
+		connections[path[1]] = append(connections[path[1]], path[0])
+	}
+	fmt.Println(explore("start", connections, map[string]bool{}))
+}
+
+func explore(start string, connections map[string][]string, alreadyVisited map[string]bool)int{
+	if start == "end"{
+		return 1
+	}
+	visited := map[string]bool{start:strings.ToLower(start)==start}
+	for node := range alreadyVisited{
+		visited[node] = alreadyVisited[node]
+	}
+	var paths int
+	for _, node := range connections[start]{
+		if ! visited[node]{
+			paths += explore(node, connections, visited)
+		}
+	} 
+	return paths
+}
